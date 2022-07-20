809f2f1696c011da45f2875df09c301b86ac4e00
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-07-20 19:38:52 +0300

    Delete src directory

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const validationData = {
-    formSelector: '.form',
-    inputSelector: '.form__field',
-    submitButtonSelector: '.form__save',
-    inactiveButtonClass: 'form__save_active',
-    inputErrorClass: 'form__field_error',
-    errorClass: 'form__field-error_active'
-};
